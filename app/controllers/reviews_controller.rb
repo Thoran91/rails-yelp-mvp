93eed65011d37e148af6646e22ec1e06f3946ec3
Tom Horan <tomhoran91@gmail.com>
2020-11-11 16:49:01 +0100

    incomplete

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,23 @@
+class ReviewsController < ApplicationController
+  #:reviews, only: [ :new, :create ]
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Creating restaurants...'
+Restaurant.create!({
+  name: "Le Dindon en Laisse",
+  address: "18 Rue Beautreillis, 75004 Paris, France",
+  category: "chinese"
+})
+Restaurant.create!({
+  name: "Neuf et Voisins",
+  address: "Van Arteveldestraat 1, 1000 Brussels, Belgium",
+  category: "chinese"
+})
+Restaurant.create!({
+  name: "McDonalds",
+  address: "Van Arteveldestraat 1, 1111 Brussels, Belgium",
+  category: "chinese"
+})
+Restaurant.create!({
+  name: "Mmaah",
+  address: "Van Arteveldestraat 1, 2341 Brussels, Belgium",
+  category: "belgian"
+})
+Restaurant.create!({
+  name: "Kebap",
+  address: "Van Arteveldestraat 1, 4566 Brussels, Belgium",
+  category: "italian"
+})
+puts 'Finished!'
